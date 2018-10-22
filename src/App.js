bcd94f333d670fcff95bdc0ea52357d4d9e3ce58
tawatchai18 <tawat1838@gmail.com>
2018-10-22 08:40:15 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,59 +1,87 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
-import { Input, Icon, Button} from 'antd';
+import { Input, Form, Button,Table,Layout,columns} from 'antd';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor( ) {
+    super()
     this.state = {
-      userName: '',
-    };
+      dataSource: []
+    }
   }
-  emitEmpty = () => {
-    this.userNameInput.focus();
-    this.setState({ userName: '' });
+
+  componentDidMount() {
+
+    axios.get('https://ffc-nectec.herokuapp.com/v0/org?query=')  
+      .then(res => console.log(res)
+      )
+      .catch(function (error) {
+    }) 
   }
-  onChangeUserName = (e) => {
-    this.setState({ userName: e.target.value });
-  }
+
   render() {
-    const { userName } = this.state;
-    const suffix = userName ? <Icon type="close-circle" onClick={this.emitEmpty} /> : null;
+    console.log(this.data);
+    
+    console.log(this.state.dataSource);
+
+    const columns = [{
+      title: 'NAME',
+      dataIndex: 'name',
+      key: 'name',
+      width: '2%'
+    },{
+      title: 'TEL',
+      dataIndex: 'tel',
+      key: 'tel',
+      width: '5%'
+    },{
+      title: 'ADDRESS',
+      dataIndex: 'address',
+      key: 'address',
+      width: '5%'
+    },{
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+      width: '5%'
+    },{
+      title: 'TYPE',
+      dataIndex: 'type',
+      key: 'type',
+      width: '5%'
+    },{
+      title: 'TIMESTAMP',
+      dataIndex: 'timestamp',
+      key: 'timestamp',
+      width: '5%'
+    }]    
+
+    const Search = Input.Search;
     return (
       <div style={{marginTop:100}}>
         <div className="App">
           <div style={{marginTop:10}}>
-            <h2>Codefin Food Center</h2>
           </div>
         </div >
         <center>
-          <b style={{marginTop:20}} className="App-intro">ข้อมูลลูกค้า</b>
-        </center>
-        <center>
-        <div style={{marginTop:50}}>  
-          <Input
-            placeholder="Enter your username"
-            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-            suffix={suffix}
-            value={userName}
-            onChange={this.onChangeUserName}
-            ref={node => this.userNameInput = node}
+        <div style={{marginTop:50,width:600}}>  
+          <Search
+            placeholder="input search text"
+            enterButton="Search"
+            size="large"
+            onSearch={value => console.log(value)}
           />
         </div>  
-        <div>
-        <Input
-            placeholder="Enter Telephone"
-            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-            suffix={suffix}
-            value={userName}
-            onChange={this.onChangeUserName}
-            ref={node => this.userNameInput = node}
-          />
-        </div>
-        <Button type="primary" onClick={this.onChangeUserName}>Primary</Button>
+        <Layout.Content style = {{margin: '24px 16px 0', overflow: 'auto', display: 'flex', flexDirection: 'row'}}>
+          <div style = {{ padding: 24, background: '#fff', 'flex': 1, minHeight: '800px', minWidth: '800px', textAlign: 'left'}}>
+            <Table dataSource = {this.state.dataSource} columns = {columns}/>
+          </div>
+       </Layout.Content>
         </center>
       </div>
     );
   }
 }
 export default App;
+
